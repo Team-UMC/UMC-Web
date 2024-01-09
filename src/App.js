6b004c42255c5427465b7e9ae96f2bd8e57c6e12
JJHyeok13 <jjinh99@naver.com>
2024-01-10 00:43:31 +0900

    feat: Calendar 구현

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './layout/header.jsx';
 import './App.css';
 import Main from './pages/main.jsx';
+import MyCalendar from './components/calendar.jsx';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <MyCalendar />
       
       <Routes>
         <Route path='/' element={<Main />} />
