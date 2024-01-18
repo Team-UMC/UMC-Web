fff1f4ad4fbbffe1f4f0a7d5c710f423c31b40e3
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-01-18 16:03:18 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -2,29 +2,24 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import 'App.css';
 
-// import Header from 'layout/Header/header';
-// import Main from 'pages/main.jsx';
-// import MyCalendar from 'components/OldBoard/calendar';
-// import BoardWrite from 'pages/BoardWrite/boardwrite';
-// import SignUpForm from 'pages/SignUp/signup';
-import BoardPage from 'pages/NewBoardPage/BoardPage';
+import Header from 'layout/header.jsx';
+import Main from 'pages/main.jsx';
+import MyCalendar from 'components/calendar.jsx';
+import BoardWrite from 'pages/boardwrite.jsx';
+import SignUpForm from 'pages/signup.jsx';
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
 
-      {/* <MyCalendar /> */}
+      <MyCalendar />
 
       <Routes>
-        <Route path="/board" element={<BoardPage />} />
-      </Routes>
-      {/* <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/boardWrite" element={<BoardWrite />} />
-        <Route path="/boardpage" element={<BoardPage />} />
         <Route path="/signup" element={<SignUpForm />} />
-      </Routes> */}
+      </Routes>
     </div>
   );
 }
