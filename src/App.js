35d428238bbc391b8185bb680ceda328260fdf4f
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-01-18 16:20:43 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import 'App.css';
 
-import Header from 'layout/header.jsx';
+// import Header from 'layout/Header/header.jsx';
 import Main from 'pages/main.jsx';
-import MyCalendar from 'components/calendar.jsx';
-import BoardWrite from 'pages/boardwrite.jsx';
-import SignUpForm from 'pages/signup.jsx';
+import BoardPage from 'pages/NewBoardPage/BoardPage';
+// import MyCalendar from 'components/OldBoard/calendar.jsx';
+// import BoardWrite from 'pages/boardwrite.jsx';
+// import SignUpForm from 'pages/SignUp/signup.jsx';
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header />
 
-      <MyCalendar />
+      <MyCalendar /> */}
 
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/boardWrite" element={<BoardWrite />} />
-        <Route path="/signup" element={<SignUpForm />} />
+        {/* <Route path="/boardWrite" element={<BoardWrite />} />
+        <Route path="/signup" element={<SignUpForm />} /> */}
+        <Route path="/board" element={<BoardPage />} />
       </Routes>
     </div>
   );
