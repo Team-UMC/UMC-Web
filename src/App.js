509a135987fc7b08f01207385398b33f0f4fea86
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-01-24 17:25:15 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
-import React, { useEffect } from 'react';
-import {
-  useRoutes,
-  useNavigate
-} from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import 'App.css';
 
-// import Header from 'layout/Header/header';
+import Header from 'layout/Header/header';
 import Main from 'pages/main.jsx';
-import BoardPage from 'pages/NewBoardPage/BoardPage';
+import BoardPageRoute from 'pages/NewBoardPage/BoardPageRoute';
 
 function App() {
-  let navigate = useNavigate();
-  let routes = useRoutes([
-    { path: '/', element: <Main /> },
-    { path: '/board/*', element: <BoardPage /> },
-    {
-      path: '/board/school',
-      element: <RedirectTo navigate={navigate} to="/board/school/notice" />,
-    },
-    {
-      path: '/board/branch',
-      element: <RedirectTo navigate={navigate} to="/board/branch/notice" />,
-    },
-    {
-      path: '/board/union',
-      element: <RedirectTo navigate={navigate} to="/board/union/notice" />,
-    },
-    {
-      path: '/board',
-      element: <RedirectTo navigate={navigate} to="/board/school/notice" />,
-    },
-  ]);
-
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
 
-      {routes}
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+
+      <BoardPageRoute />
     </div>
   );
 }
-
-const RedirectTo = ({ navigate, to }) => {
-  useEffect(() => {
-    navigate(to);
-  }, [navigate, to]);
-
-  return null;
-};
-
-RedirectTo.propTypes = {
-  navigate: PropTypes.func.isRequired,
-  to: PropTypes.string.isRequired,
-};
 
 export default App;
