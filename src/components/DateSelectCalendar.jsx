d68347ae29f8da002d5b18a3eb191ce6ac329e11
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-02-13 15:33:38 +0900

    fix

--- src/components/DateSelectCalendar.jsx
+++ src/components/DateSelectCalendar.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
-import { getDay, getMonth, getYear, isToday, setMonth } from 'date-fns';
+import {
+  getDay,
+  getMonth,
+  getYear,
+  isToday,
+  subMonths,
+  addMonths,
+} from 'date-fns';
 import 'components/dateselectcalendar.css';
 
 import CalendarIcon from 'assets/main/calendar.svg';
@@ -24,12 +31,6 @@ const MONTHS = [
 
 const DateSelectCalendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [monthSelectMode, setMonthSelectMode] = useState(false);
-
-  const handleMonthClick = (monthIndex) => {
-    setSelectedDate(setMonth(selectedDate, monthIndex));
-    setMonthSelectMode(false);
-  };
 
   return (
     <DatePicker
@@ -38,6 +39,7 @@ const DateSelectCalendar = () => {
       formatWeekDay={(nameOfDay) => nameOfDay.substring(0, 3).toUpperCase()} // 요일 형태 (ex. 'Mon')
       showYearDropdown // 년도 선택 가능
       scrollableYearDropdown // 년도 선택 시 스크롤 가능
+      showMonthDropdown // 월 선택 가능
       shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
       selected={selectedDate} // 선택된 날짜
       calendarClassName="calendar-date-select"
@@ -69,32 +71,28 @@ const DateSelectCalendar = () => {
             <img
               src={LeftArrow}
               alt="left-arrow"
-              onClick={decreaseMonth}
+              onClick={() => {
+                decreaseMonth();
+                setSelectedDate(subMonths(selectedDate, 1));
+              }}
               className={prevMonthButtonDisabled ? 'disabled' : ''}
             />
           </div>
           <div className="calendar-header-center">
             <img src={CalendarIcon} alt="calendar-icon" />
-            {monthSelectMode ? (
-              <div>
-                {MONTHS.map((month, index) => (
-                  <div key={month} onClick={() => handleMonthClick(index)}>
-                    {month}
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <div onClick={() => setMonthSelectMode(true)}>
-                <div>{MONTHS[getMonth(date)]}</div>
-                <div>{getYear(date)}</div>
-              </div>
-            )}
+            <div className="calendar-header-month">
+              <div>{MONTHS[getMonth(date)]}</div>
+              <div>{getYear(date)}</div>
+            </div>
           </div>
           <div className="calendar-header-right">
             <img
               src={RightArrow}
               alt="right-arrow"
-              onClick={increaseMonth}
+              onClick={() => {
+                increaseMonth();
+                setSelectedDate(addMonths(selectedDate, 1));
+              }}
               className={nextMonthButtonDisabled ? 'disabled' : ''}
             />
           </div>
